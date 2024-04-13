003aa0107d3197e5a46e3c4a364636dca5b6876c
Literally Void <literallyvoid@gmail.com>
2024-04-12 17:29:14 -0700

    Change canvas resize logic to avoid fraction pixel size when the window isn't a round number of pixels

--- src/gfx.js
+++ src/gfx.js
@@ -4,8 +4,11 @@ export const ctx = canvas.getContext("2d");
 function resizeCanvas() {
 	const dpr = window.devicePixelRatio ?? 1;
 
-	canvas.width = canvas.clientWidth * dpr;
-	canvas.height = canvas.clientHeight * dpr;
+	canvas.width = window.innerWidth * dpr;
+	canvas.height = window.innerHeight * dpr;
+
+	canvas.style.width = (canvas.width / dpr) + 'px';
+	canvas.style.height = (canvas.height / dpr) + 'px';
 }
 
 window.addEventListener("resize", resizeCanvas);
