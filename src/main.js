9ab98fcb4a05a02c73d090a4e2b7ac834b3e1581
Literally Void <literallyvoid@gmail.com>
2024-04-13 22:38:17 -0700

    Move start of track down half a unit in preparation for pathfinding

--- src/main.js
+++ src/main.js
@@ -397,7 +397,8 @@ class Board {
 			}
 
 			if (y == 0) {
-				this.enemyTrack.push([x, y - 1]);
+				// The first track item is on the edge of the grid: 0.5 units above the first proper grid cell.
+				this.enemyTrack.push([x, y - 0.5]);
 			}
 			this.enemyTrack.push([x, y]);
 
